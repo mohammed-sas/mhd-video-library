1698710d952c018f8301ff83211f79801490bae0
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-03-24 12:18:35 +0530

    feat : signup screen

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -19,40 +19,40 @@ const Signup = () => {
     }
     return (
         <main className="login-container">
-                <div className="login-form-container bg-white ">
-                    <h2 className="centered-text grey">Signup</h2>
-                    <form action="post" className="login-form" onSubmit={submitHandler}>
-                        <label htmlFor="email">Email address 
+                <div className="login-form-container bg-black ">
+                    <h2 className="centered-text text-white">Signup</h2>
+                    <form action="post" className="login-form" >
+                        <label className="text-white" htmlFor="email">Email address 
                             <br/> <input id="email" name="email" onChange={changeHandler} type="email" placeholder="abc@neog.com"/>
                         </label>
-                        <label htmlFor="fname">First Name 
+                        <label className="text-white" htmlFor="fname">First Name 
                             <br/> <input id="firstName" name="fname" required onChange={changeHandler} type="text" placeholder="First Name"/>
                         </label>
-                        <label htmlFor="lname">Last Name 
+                        <label className="text-white" htmlFor="lname">Last Name 
                             <br/> <input id="lastName" name="lname" required onChange={changeHandler} type="text" placeholder="Last Name"/>
                         </label>
                         
-                        <label htmlFor="password">Password
+                        <label className="text-white" htmlFor="password">Password
                             <div id="password" className="password">
-                                <input name="password" required type={showpass ?"text":"password"}onChange={changeHandler} />
-                                <i onClick={setShowpass} className={"fas " + (showpass?"fa-eye":"fa-eye-slash")}></i>
+                                <input name="password" required onChange={changeHandler} />
+                            
                             </div>
                         </label>
-                        <label  htmlFor="confirmPassword">Confirm Password
+                        <label className="text-white"  htmlFor="confirmPassword">Confirm Password
                             <div className="password">
-                                <input name="confirmPassword" required type={showConfirmpass ?"text":"password"} onChange={changeHandler} />
-                                <i onClick={setShowConfirmpass} className={"fas " + (showConfirmpass?"fa-eye":"fa-eye-slash")}></i>
+                                <input name="confirmPassword" required  onChange={changeHandler} />
+                                
                             </div>
                         </label>
-                        {passmatch?null: <span className="mismatch">Passwords Not Matching</span>}
+                        
                         
                         <div>
-                            <label htmlFor="accept-condition"><input id="accept-condition" type="checkbox"/> I accept all Terms & Conditions</label>
+                            <label htmlFor="accept-condition" className="text-white"><input id="accept-condition" type="checkbox"/> I accept all Terms & Conditions</label>
                             
                         </div>
-                        <input type="submit" value="Create New Account"  className="btn btn-primary"/>
+                        <input type="submit" value="Create New Account"  className="btn btn-primary bg-primary text-grey"/>
                         <div>
-                            <p className="centered-text"><Link to="/login">Already have an account <i className="fas fa-chevron-right"></i></Link></p>
+                            <p className="centered-text"><Link to="/login" className="text-primary">Already have an account <i className="fas fa-chevron-right"></i></Link></p>
                         </div>
                     </form>
                 </div>
