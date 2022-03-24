1c321077199e62fdbd4ae4f132baeb7a0153e8ac
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-03-24 23:23:31 +0530

    feat : added products for trickshot and defense categories

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,146 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "2dadfb6c-ab96-11ec-b909-0242ac120002",
+    youtubeId:"xC5uc5uATGc",
+    title: "Badminton shots from 2021 - Top 10",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "TOP 10 Badminton shots from 2021. Welcome to our top 10 shots from 2021; we hope you enjoy them. We have selected 10 of our favorite shots, deceptions, and trick shots from 2021. Returns, drives, defense, tricks, cut, backhand & more",
+    videoThumbnail: "https://i.ytimg.com/vi/xC5uc5uATGc/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"trickshot",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj"
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "2dadfe82-ab96-11ec-b909-0242ac120002",
+    youtubeId:"zrGh10iTwX0",
+    title: "SPIN SERVE in badminton - 2 techniques",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "SPIN SERVE in badminton - 2 techniques. The spin serve in badminton is a tricky shot to master, and you see more and more players do them. Kevin Sukamuljo was the first to introduce it without hitting the feathers. Sukamuljo puts so much spin into the serve, and I will try to show you how this is applied. I will show you 1 shot with a bit spin which is easier to apply and use, and then the big crazy spin serve as Kevin Sukamuljo does. Welcome to the badminton spin serve trick tutorial",
+    videoThumbnail: "https://i.ytimg.com/vi/zrGh10iTwX0/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"trickshot",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
   },
+  {
+    _id: "2dae0332-ab96-11ec-b909-0242ac120002",
+    youtubeId:"TiTm80LX4uU",
+    title: "Badminton Tricks From Backcourt",
+    description:
+      "Badminton Tricks From Backcourt. Welcome to another trickshot video here on the channel. 3 trickshots from backcourt. They are funny, crazy and hard to do. They are mostly for fun, but 1 of them can actually be used in a real match, watch and see. We love tricks because when you have fun and try new stuff, you will learn new things and develop your skills with the racket.",
+    videoThumbnail: "https://i.ytimg.com/vi/TiTm80LX4uU/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"trickshot",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae0490-ab96-11ec-b909-0242ac120002",
+    youtubeId:"5DOgzV8l30s",
+    title: "Badminton LIN DAN trickshot - Back facing shot",
+    description:
+      "Badminton LIN DAN trickshot - Back facing shot. This amazing tutorial back facing trickshot from Lin Dan is a MUST SEE, and a shot you must try yourself. Lin Dan is SO good at this fake backhand shot, I give my best but Lin Dan is just the master of this trick shot in badminton. To do a back facing shot takes skills, and only Lin Dan can pull it off with style.",
+    videoThumbnail: "https://i.ytimg.com/vi/5DOgzV8l30s/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"trickshot",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae05da-ab96-11ec-b909-0242ac120002",
+    youtubeId:"9r9lu_kBWYQ",
+    title: "Badminton challenge - Low serve, how many can you get?",
+    description:
+      "Badminton challenge - Low serve, how many can you get? Badminton low serve is used a lot in badminton, so master the short serve and also focus on the precision. Attach a racket on the edge of the net and do low serves precision through the rackethead. The badminton serve is an important element in badminton.",
+    videoThumbnail: "https://i.ytimg.com/vi/9r9lu_kBWYQ/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"trickshot",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae0742-ab96-11ec-b909-0242ac120002",
+    youtubeId:"eNFBYQBwkg8",
+    title: "Badminton TRICKSHOT - WOW Behind the head",
+    description:
+      "Badminton trickshot behind the head. This trickshot is SO crazy. A badminton trick you MUST try yourself, really funny trick, but also mostly for fun. Trickshots in badminton is mostly for fun, because the speed of the game is so fast. But fun to practise and still some body control practise in these shots for sure, so give it a try.🤩",
+    videoThumbnail: "https://i.ytimg.com/vi/eNFBYQBwkg8/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"trickshot",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae0882-ab96-11ec-b909-0242ac120002",
+    youtubeId:"ONnr77d4dQQ",
+    title: "Badminton Doubles Defense - Cross short",
+    description:
+      "Badminton Doubles Defense - Cross short. Hope you are ready for a tricky shot, this is the doubles defense going short cross from the right field. So a Badminton defense block, where you aim for attacking the net, and changing things around. Use the basic double defense technique, but do not hit will full power, use a small finger power push and a forearm rotation from outside and into the shuttle. Watch and learn in this Badminton Doubles Defense cross short technique tutorial 😎",
+    videoThumbnail: "https://i.ytimg.com/vi/ONnr77d4dQQ/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"defense",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae09a4-ab96-11ec-b909-0242ac120002",
+    youtubeId:"1wuoT8AdFsM",
+    title: "Badminton singles defense - Cross in Forehand and Backhand",
+    description:
+      "Badminton singles defense - Cross in Forehand and Backhand. Welcome to this how to tutorial about the cross defense in singles. I go through the defense footwork, the footwork where you are in control, not under full heavy pressure. Then I look at the techniques in the forehand and in the backhand. I talk about the grip, the wrist, the forearm, the angle and the finger power. Hope you enjoy this badminton singles defense tutorial 😎",
+    videoThumbnail: "https://i.ytimg.com/vi/1wuoT8AdFsM/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"defense",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae0ac6-ab96-11ec-b909-0242ac120002",
+    youtubeId:"sJcWuXcH7kQ",
+    title: "Badminton Net Kill - Forehand 2 versions",
+    description:
+      "Badminton Net Kill - Forehand 2 versions and also the backhand version. The badminton net kill is very important to master to kill the shuttles at the net and score points. I cover the forehand version and also shows you the backhand version in this tutorial. The net kill forehand exercises can be where you throw to the net or multishuttle to the net. Go for the fast quick kill if you have time using fingerpower, or use the wiperblade move if you are close to the net.",
+    videoThumbnail: "https://i.ytimg.com/vi/sJcWuXcH7kQ/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"defense",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae0eb8-ab96-11ec-b909-0242ac120002",
+    youtubeId:"gIynLyyY0zU",
+    title: "Badminton net kill - Doubles net player",
+    description:
+      "Badminton net kill - Doubles net player. Welcome to this net kill tutorial as the net player in the doubles. I will walk you through the footwork at the net, the technique of the net kill, and how to train this together with a feeder or a shuttle machine. It´s all about low gravity, racket ready, forehand grip, forearm rotation, and the quick kill using the kickback movement",
+    videoThumbnail: "https://i.ytimg.com/vi/gIynLyyY0zU/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"defense",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae11ec-ab96-11ec-b909-0242ac120002",
+    youtubeId:"CxVB0iIjvuU",
+    title: "Badminton backhand slice cross court",
+    description:
+      "Badminton backhand slice cross court. This late backhand slice going cross is brutal to master but yet essential to learn. The technique in the shot is very similar to the straight slice; make sure you watch our tutorial on that shot. The backhand slice is a tricky shot with focus on grip, wrist, forearm rotation, elbow, and the follow-through",
+    videoThumbnail: "https://i.ytimg.com/vi/CxVB0iIjvuU/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"defense",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae13a4-ab96-11ec-b909-0242ac120002",
+    youtubeId:"2GMSxlRqdQ0",
+    title: "Badminton Cross Shots | Must Learn deadzone shots",
+    description:
+      "Badminton Cross Shots | Must Learn deadzone shots. These cross shots in badminton is key to master to make awesome deadzone shots. The cross forehand and cross backhand shots from deadzone, is used many times in singles and in mixed doubles. This tutorial focuses on the version very close to the net, and also the version from deadzone to deadzone, forehand and backhand in both sides",
+    videoThumbnail: "https://i.ytimg.com/vi/2GMSxlRqdQ0/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"defense",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  },
+  {
+    _id: "2dae1520-ab96-11ec-b909-0242ac120002",
+    youtubeId:"Un6YmO1gBK8",
+    title: "Badminton double defense - short to the net",
+    description:
+      "Badminton double defense - short to the net tutorial. Are you ready to learn the badminton double defense where you slice the shuttle just a bit? The double defense technique in badminton is very important, and this short defense is a good variation to change things around in the game. Play it short to the net on the smash close to the net, that is a good badminton double defense.",
+    videoThumbnail: "https://i.ytimg.com/vi/Un6YmO1gBK8/mqdefault.jpg",
+    channelTitle:"Badminton Famly",
+    catgory:"defense",
+    channelThumbnail:"https://yt3.ggpht.com/ytc/AKedOLQ8uUyZP-fdAtjaiqpp6mcdJWIxyAOlFU7BCwZHIw=s88-c-k-c0x00ffffff-no-rj" 
+  }
 ];
