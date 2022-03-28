6e8f9bcec62fcbfb7570ff63e66afc600306ad25
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-03-29 00:43:32 +0530

    fix : navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,15 +2,16 @@ import logo from "../../assets/logo.webp";
 import classes from "./navbar.module.css";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../context/auth-context";
+import {useToggle} from '../../hooks/useToggle';
 const Navbar = () => {
   const { currentUser,logout } = useAuth();
-
+  const [showSideBar,setShowSidebar] = useToggle(false);
   const logoutHandler=()=>{
     logout();
   }
   return (
     <nav className="nav-bar bg-black bottom-shadow width-auto">
-      <i className="fas fa-bars drawer-btn"></i>
+      <i className={`fas fa-bars drawer-btn ${classes["hamburger-icon"]}`} onClick={setShowSidebar}></i>
       <div className={`nav-brand ${classes["nav-brand-lib"]}`}>
         <h2 className="text-primary">MHD</h2>
         <small className="text-primary">Video Library</small>
@@ -23,7 +24,7 @@ const Navbar = () => {
           placeholder="Search..."
         />
       </div>
-      <div className="nav-links">
+      <div id={classes["side-bar"]} className={`nav-links ${(showSideBar ? classes["active"] : "")}`}>
         <ul>
           <li>
             {currentUser ? (
