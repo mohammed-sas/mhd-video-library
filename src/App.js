5f81c68266c27df3c1f7e12d2777360fbece9b10
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-03-30 16:21:24 +0530

    barrel export pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import { Routes, Route } from "react-router";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup";
-import VideoListing from "./pages/video listing/VideoListing";
-import Playlist from "./pages/playlist/Playlist";
-import SpecificPlaylist from "./pages/playlist/specific playlist/SpecificPlaylist";
 import Mockman from "mockman-js";
-import Liked from "./pages/liked/Liked";
-import SingleVideo from "./pages/single video/SingleVideo";
-import History from "./pages/history/History";
-import WatchLater from "./pages/watch later/WatchLater";
-
+import {
+  Home,
+  Login,
+  Signup,
+  VideoListing,
+  Playlist,
+  Liked,
+  SingleVideo,
+  History,
+  WatchLater,
+  SpecificPlaylist
+} from "./pages";
 function App() {
   return (
     <div className="App bg-black">
@@ -24,10 +25,10 @@ function App() {
         <Route path="/explore" element={<VideoListing />} />
         <Route path="/playlists" element={<Playlist />} />
         <Route path="/playlists/:playlistId" element={<SpecificPlaylist />} />
-        <Route path="/liked" element={<Liked/>}/>
-        <Route path="/explore/:videoId" element={<SingleVideo/>}/>
-        <Route path ="/history" element={<History/>}/>
-        <Route path="/watchlater" element={<WatchLater/>}/>
+        <Route path="/liked" element={<Liked />} />
+        <Route path="/explore/:videoId" element={<SingleVideo />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/watchlater" element={<WatchLater />} />
         <Route path="/mock-api" element={<Mockman />} />
       </Routes>
     </div>
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import History from './history/History';
+import Home from './home/Home';
+import Liked from './liked/Liked';
+import Login from './login/Login';
+import Playlist from './playlist/Playlist';
+import Signup from './signup/Signup';
+import SingleVideo from './single video/SingleVideo';
+import VideoListing from './video listing/VideoListing';
+import WatchLater from './watch later/WatchLater';
+import SpecificPlaylist from './playlist/specific playlist/SpecificPlaylist';
+export {History,Home,Liked,Login,Playlist,Signup,SingleVideo,VideoListing,WatchLater,SpecificPlaylist};
