fbcba0085fbf1c57c78609d5b678b32229a5a928
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-03-31 17:19:59 +0530

    feat : search based on title and category

--- src/components/navbar/searchbar/Searchbar.jsx
+++ src/components/navbar/searchbar/Searchbar.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import { useVideo } from "../../../context/";
-import classes from './searchBar.module.css'
+import classes from "./searchBar.module.css";
 
 const Searchbar = () => {
-    const {videoLists} = useVideo();
-    const [searchList,setSearchList] = useState([]);
-    const changeHandler=e=>{
-        let query = e.target.value.trim();
-        let lists = videoLists.videos.filter(video=>video.category === query);
-        setSearchList(lists);
+  const { videoLists } = useVideo();
+  const [searchList, setSearchList] = useState([]);
+  const navigate = useNavigate();
+  const changeHandler = (e) => {
+    let searchString = e.target.value.trim().toLowerCase();
+    let queryLen = searchString.length;
+    if (queryLen === 0) {
+      setSearchList([]);
+      return;
     }
+    let lists = videoLists.videos.filter((video) => {
+      return (
+        video.category.substring(0, queryLen) === searchString ||
+        video.title.toLowerCase().match(searchString)
+      );
+    });
+    setSearchList(lists);
+  };
+
+  const clickHandler = (id) => {
+    navigate(`/explore/${id}`);
+    setSearchList([]);
+  };
   return (
     <div className={`search-bar ${classes["position-relative"]}`}>
       <i className="fas fa-search text-primary"></i>
@@ -20,16 +37,14 @@ const Searchbar = () => {
         onChange={changeHandler}
       />
       <ul className={classes["search-lists"]}>
-          {
-              searchList.map(video=>{
-                  return(
-                      <li>
-                          <p className="text-white">{video.title}</p>
-                          <small className="text-white">in {video.category}</small>
-                      </li>
-                  )
-              })
-          }
+        {searchList.map((video) => {
+          return (
+            <li key={video._id} onClick={() => clickHandler(video._id)}>
+              <p className="text-white">{video.title}</p>
+              <small className="text-white">in {video.category}</small>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
