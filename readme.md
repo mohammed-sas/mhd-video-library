bd72a91d8c099907e5de853808c1a33419d63721
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-04-07 13:45:06 +0530

    feat:readme and signup validation

--- readme.md
+++ readme.md
@@ -1,3 +1,51 @@
-## Video Library Backend
 
-This folder contains the Mock Backend created for Video Library App. It contains different API end-points and database configuration for building Video Library; so that you can focus on building Frontend.
+# MHD Video Library
+
+The complete tutorial video library where you could find great tutorials related to badmintion trickshots, defense , footwork and bio-mechanics
+## Tech Stack
+1.React   
+2.React context API +  useReducer hook  
+3.React Router v6  
+4.MHD UI Component Library  
+5.CSS modules
+## Functionalities
+1. Video listing page
+1. Authentication
+    * Login  
+    * Signup  
+    * Logout
+1. Playlist management
+    * Create new playlist  
+    * Add videos to playlist  
+    * Remove videos from playlist  
+    * Delete playlist  
+1. History management
+    * Add to history 
+    * Clear specific video from history  
+    * Clear all history  
+1. Watchlater management
+    * Add to Watchlater 
+    * Remove from Watchlater  
+1. Like management
+    * Add to liked videos 
+    * Remove from liked videos  
+1. Notes 
+    * Add multiple notes to videos with timestamp
+    * Edit note's title and description  
+1. Singe Video Page  
+1. Filter by categories  
+1. Search videos   
+1. User profile page  
+1. Loader,toast and error page  
+    
+
+    
+
+          
+
+## Authors
+
+- Github - [@mohammed-sas](https://www.github.com/mohammed-sas)
+- Twitter-[@mhdsas](https://twitter.com/mhdsas)
+- LinkedIn-[mohammed ashraf](https://www.linkedin.com/in/mohammed-ashraf-ba0a11133/)
+
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -9,7 +9,8 @@ const Signup = () => {
     const [showpass,setShowpass] = useToggle(false);
     const [showConfirmpass,setShowConfirmpass] = useToggle(false);
     const {signUp} = useAuth();
-    const [passMatch,setPassMatch] = useState(false);
+    const [passMatch,setPassMatch] = useState(true);
+    const [passLen,setPassLen] = useState(true);
     const [user,setUser] = useState({
         email:"",
         firstName:"",
@@ -31,6 +32,10 @@ const Signup = () => {
                 setPassMatch(false);
                 return;
             }
+            if(user.password.length<8){
+                setPassLen(false);
+                return;
+            }
             const status = await signUp(user);
             if(status===201)
                 navigate("/");
@@ -43,33 +48,33 @@ const Signup = () => {
                 <div className="login-form-container bg-black ">
                     <h2 className="centered-text text-white">Signup</h2>
                     <form action="post" className="login-form" onSubmit={submitHandler}>
-                        <label className="text-white" htmlFor="email">Email address 
-                            <br/> <input id="email" name="email" onChange={changeHandler} type="email" placeholder="abc@neog.com"/>
+                        <label className="text-white" htmlFor="email"><span>Email address</span> 
+                             <input required id="email" name="email" onChange={changeHandler} type="email" placeholder="abc@neog.com"/>
                         </label>
-                        <label className="text-white" htmlFor="firstName">First Name 
-                            <br/> <input id="firstName" name="firstName" required onChange={changeHandler} type="text" placeholder="First Name"/>
+                        <label className="text-white" htmlFor="firstName"><span>First Name</span> 
+                            <input  id="firstName" name="firstName" required onChange={changeHandler} type="text" placeholder="First Name"/>
                         </label>
-                        <label className="text-white" htmlFor="lastName">Last Name 
-                            <br/> <input id="lastName" name="lastName" required onChange={changeHandler} type="text" placeholder="Last Name"/>
+                        <label className="text-white" htmlFor="lastName"><span>Last Name</span> 
+                             <input id="lastName" name="lastName" required onChange={changeHandler} type="text" placeholder="Last Name"/>
                         </label>
                         
-                        <label className="text-white" htmlFor="password">Password
+                        <label className="text-white" htmlFor="password"><span>Password</span>
                             <div id="password" className="password">
                                 <input name="password" type={showpass ?"text":"password"} required onChange={changeHandler} />
                                 <i onClick={setShowpass} className={"fas " + (showpass?"fa-eye":"fa-eye-slash")}></i>
                             </div>
                         </label>
-                        <label className="text-white"  htmlFor="confirmPassword">Confirm Password
+                        <label className="text-white"  htmlFor="confirmPassword"><span>Confirm Password</span>
                             <div className="password">
                                 <input name="confirmPassword" type={showConfirmpass ?"text":"password"} required  onChange={changeHandler} />
                                 <i onClick={setShowConfirmpass} className={"fas " + (showConfirmpass?"fa-eye":"fa-eye-slash")}></i>
                             </div>
                         </label>
                         {passMatch?null: <span className="mismatch">Passwords Not Matching</span>}
-                        
+                        {passLen? null :<span className="mismatch">Password must have minimum of 8 characters</span>}
                         
                         <div>
-                            <label htmlFor="accept-condition" className="text-white"><input id="accept-condition" type="checkbox"/> I accept all Terms & Conditions</label>
+                            <label htmlFor="accept-condition" className="text-white"><input id="accept-condition" required type="checkbox"/> I accept all Terms & Conditions</label>
                             
                         </div>
                         <input type="submit" value="Create New Account"  className="btn btn-primary bg-primary text-grey"/>
